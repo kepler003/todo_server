cea946501f2bf45e10a27937b9607966b441f65d
Bartłomiej Pyzocha <b.pyzocha@gmail.com>
2020-09-24 12:50:05 +0200

    Set up test router

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-})
+app.use(require('./routes/index.js'));
 
 app.listen(PORT, () => {
   console.log(`Server is up at port ${PORT}`);
