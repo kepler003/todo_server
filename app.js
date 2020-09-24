99177a5ad2d2aaa2c30d9771d8744b03371b336e
Bartłomiej Pyzocha <b.pyzocha@gmail.com>
2020-09-24 12:41:12 +0200

    Basic server setup

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is up at port ${PORT}`);
+})
